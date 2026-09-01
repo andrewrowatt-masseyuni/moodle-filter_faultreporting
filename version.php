b778efa494aa2a98f877e8529158b84106d7d439
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2026-09-02 10:51:35 +1200

    Resolves externallib_test::test_get_courses_by_field failing

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'filter_faultreporting';
 $plugin->release      = '1.0';
-$plugin->version      = 2026080100;
+$plugin->version      = 2026090200;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 501];
 $plugin->maturity     = MATURITY_STABLE;
